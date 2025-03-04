d20cab234adf419d8bb7fcd41f54edce04172440
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2025-03-04 09:37:36 +0300

    add the environment variables

--- server/src/main/java/com/example/echoQuest/EchoQuestApplication.java
+++ server/src/main/java/com/example/echoQuest/EchoQuestApplication.java
@@ -1,5 +1,6 @@
 package com.example.echoQuest;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EchoQuestApplication {
 
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+    System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+    System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+    System.setProperty("SECURITY_USER_PASSWORD", dotenv.get("SECURITY_USER_PASSWORD"));
+    System.setProperty("SECURITY_USER_NAME", dotenv.get("SECURITY_USER_NAME"));
     SpringApplication.run(EchoQuestApplication.class, args);
   }
 }
